05f2a1a53f63d9b90ff097310cdd85407a8a0b66
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-07-18 00:25:55 -0300

    feat: add Course 02 - Tuple in Python

---
+++ Estruturas De Dados/Curso 02 - Tuplas/declaracao_tupla.py
@@ -0,0 +1,12 @@
+
+# Declaração da Tupla
+frutas = ("laranja", "pera", "uva")
+letra = tuple("python")
+numeros = tuple([1, 2, 3, 4])
+pais = ("Brasil",)
+
+# Impressão da Tupla
+print(frutas)
+print(letra)
+print(numeros)
+print(pais)
---
+++ Estruturas De Dados/Curso 02 - Tuplas/fatiamento.py
@@ -0,0 +1,8 @@
+tupla = ("p", "y", "t", "o", "n")
+
+print(tupla[2:])  # ('t', 'o', 'n')
+print(tupla[:2])  # ('p', 'y')
+print(tupla[1:3])  # ('y', 't')
+print(tupla[0:3:2])  # ('p', 't')
+print(tupla[::])  # ('p', 'y', 't', 'o', 'n')
+print(tupla[::-1])  # ('n', 'o', 't', 'y', 'p')
